24ef9ae2575e2deec8085621e441f95f0f34deeb
xLsDg <xlsdg@qq.com>
2017-01-14 00:01:49 +0800

    chore(build): change uglifyjs config

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -29,8 +29,13 @@ var webpackConfig = merge(baseWebpackConfig, {
       'process.env': "'production'"
     }),
     new webpack.optimize.UglifyJsPlugin({
+      beautify: false,  // 最紧凑的输出
+      comments: false,  // 删除所有的注释
       compress: {
-        warnings: false
+        warnings: false,  // 在UglifyJs删除没有用到的代码时不输出警告
+        drop_console: true,  // 删除所有的 `console` 语句 还可以兼容ie浏览器
+        collapse_vars: true,  // 内嵌定义了但是只用到一次的变量
+        reduce_vars: true  // 提取出出现多次但是没有定义成变量去引用的静态值
       }
     }),
     new webpack.optimize.OccurenceOrderPlugin(),
